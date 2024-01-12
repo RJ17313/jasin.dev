e8db968888c3398cff8f9c8e2702e47d55725c29
Ryan <github@jasin.dev>
2024-01-12 14:46:04 -0500

    card progress

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,44 @@
-import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Image,
+} from "@nextui-org/react";
 import Circle from "./Circle";
 
 export default function ProjectCard(props) {
   return (
     <Card className="max-w-64">
       <CardHeader>
-        <p className="font-bold">{props.title}</p>
+        <div className="flex items-center gap-2">
+          <Image
+            isBlurred
+            alt="GitHub Logo"
+            src="github-mark-white.svg"
+            className="h-8 w-8"
+          ></Image>
+          <p className="font-bold">{props.title}</p>
+        </div>
       </CardHeader>
       <CardBody>
         <p>{props.description}</p>
       </CardBody>
       <CardFooter>
-        <div className="inline-flex items-center gap-1">
-          <Circle />
-          <p>{props.primaryLanguage}</p>
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center gap-1">
+            <Circle />
+            <p>{props.primaryLanguage}</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <Image
+              alt="Star Icon"
+              src="star.svg"
+              className="h-5 w-5"
+              isBlurred
+            ></Image>
+            <p className="font-bold">20</p>
+          </div>
         </div>
       </CardFooter>
     </Card>
